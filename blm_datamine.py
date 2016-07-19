fae22be97d0012e4599bde65b59298c0ccabf245
Brant Abeln <baabeln@ucdavis.edu>
2016-07-18 19:30:58 -0500

    Upload of plots and blm

--- blm_datamine.py
+++ blm_datamine.py
@@ -4,6 +4,8 @@
 # before and after the recent shooting in Baton Rouge.
 from twython import Twython
 import json
+import pandas, time
+import numpy
 
 # Twitter keys
 CONSUMER_KEY = 'szzIqYXh0j4KsYiLcBfltVSEx'
@@ -11,46 +13,171 @@ CONSUMER_SECRET = 'yO3h76bky5fKxfFUI4zb0KEejxEEVDrKaBmOMsd0rM3HbhcKzk'
 OAUTH_TOKEN = '18917349-kkp3XQ4uszMObYURy2AVrsVDf06KR6fZOgRdALpEF'
 OAUTH_TOKEN_SECRET = 'jlZvzVsTsYdigqVY7lGNlnDjDoBqOEGzr2quuStbMmhAF'
 
-# Connect to the twiter API
+# Connect to the Twiter API
 twitter_api = Twython(CONSUMER_KEY, CONSUMER_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
 
-
+# Find the hashtag #BLM
+# Twitter only allows you to grab 100 tweets at a time.
 hashtag = '#BLM'
 count = 100
 
-search_results = twitter_api.search(q=hashtag, count=count, until='2016-07-18')
-tweets = search_results['statuses']
+tweets=[]
 
-for i in range(0,100):
+# Loop to collect as many tweets as Twitter will give me
+for i in range(0,50):
 
-
-    #----------------------------------------------------------------#
-    # STEP 1: Query Twitter
-    # STEP 2: Save the returned tweets
-    # STEP 3: Get the next max_id
-    #----------------------------------------------------------------#
-
-    # STEP 1: Query Twitter
+    # Collect tweets from Sunday night backwards
     if(0 == i):
-        # Query twitter for data. 
-        search_results = twitter_api.search(q=hashtag, count=count, until='2016-07-18')
+        try:
+            search_results = twitter_api.search(q=hashtag, count=count, until='2016-07-18')
+        except error:
+            break
     else:
         # After the first call we should have max_id from result of previous call. Pass it in query.
-        search_results = twitter_api.search(q=hashtag,include_entities='true',max_id=next_max_id, count=count, until='2016-07-18')
+        try:
+            search_results = twitter_api.search(q=hashtag,include_entities='true',max_id=next_id, count=count, until='2016-07-18')
+        except:
+            break
 
-    # STEP 2: Save the returned tweets
+    # Save tweets
     for result in search_results['statuses']:
         tweets.append(result)
 
 
-    # STEP 3: Get the next max_id
+    # Extract the last tweet given to reseed the request
     try:
-        # Parse the data returned to get max_id to be passed in consequent call.
-        next_results_url_params = search_results['search_metadata']['next_results']
-        next_max_id = next_results_url_params.split('max_id=')[1].split('&')[0]
+        oldest = search_results['search_metadata']['next_results']
+        next_id = oldest.split('max_id=')[1].split('&')[0]
     except:
-        # No more next pages
         break
 
-# Show one sample search result by slicing the list...
-#print(json.dumps(statuses[0], indent=1))
+# Collect tweets with hashtag #blacklivesmatter
+hashtag = '#blacklivesmatter'
+
+# Loop to collect as many tweets as Twitter will give me
+for i in range(0,50):
+
+    # Collect tweets from Sunday night backwards
+    if(0 == i):
+        try:
+            search_results = twitter_api.search(q=hashtag, count=count, until='2016-07-18')
+        except error:
+            break
+    else:
+        # After the first call we should have max_id from result of previous call. Pass it in query.
+        try:
+            search_results = twitter_api.search(q=hashtag,include_entities='true',max_id=next_id, count=count, until='2016-07-18')
+        except:
+            break
+        
+    # Save tweets
+    for result in search_results['statuses']:
+        tweets.append(result)
+
+
+    # Extract the last tweet given to reseed the request
+    try:
+        oldest = search_results['search_metadata']['next_results']
+        next_id = oldest.split('max_id=')[1].split('&')[0]
+    except:
+        break
+
+
+
+
+    
+# Collect tweets from before the Baton Rouge incident
+# Find the hashtag #BLM
+# Twitter only allows you to grab 100 tweets at a time.
+hashtag = '#BLM'
+
+# Loop to collect as many tweets as Twitter will give me
+for i in range(0,50):
+
+    # Collect tweets from Sunday night backwards
+    if(0 == i):
+        try:
+            search_results = twitter_api.search(q=hashtag, count=count, until='2016-07-17')
+        except error:
+            break
+    else:
+        # After the first call we should have max_id from result of previous call. Pass it in query.
+        try:
+            search_results = twitter_api.search(q=hashtag,include_entities='true',max_id=next_id, count=count, until='2016-07-17')
+        except:
+            break
+        
+    # Save tweets
+    for result in search_results['statuses']:
+        tweets.append(result)
+
+
+    # Extract the last tweet given to reseed the request
+    try:
+        oldest = search_results['search_metadata']['next_results']
+        next_id = oldest.split('max_id=')[1].split('&')[0]
+    except:
+        break
+
+# Collect tweets with hashtag #blacklivesmatter
+hashtag = '#blacklivesmatter'
+
+# Loop to collect as many tweets as Twitter will give me
+for i in range(0,50):
+
+    # Collect tweets from Sunday night backwards
+    if(0 == i):
+        try:
+            search_results = twitter_api.search(q=hashtag, count=count, until='2016-07-17')
+        except error:
+            break
+    else:
+        # After the first call we should have max_id from result of previous call. Pass it in query.
+        try:
+            search_results = twitter_api.search(q=hashtag,include_entities='true',max_id=next_id, count=count, until='2016-07-17')
+        except:
+            break
+        
+    # Save tweets
+    for result in search_results['statuses']:
+        tweets.append(result)
+
+
+    # Extract the last tweet given to reseed the request
+    try:
+        oldest = search_results['search_metadata']['next_results']
+        next_id = oldest.split('max_id=')[1].split('&')[0]
+    except:
+        break
+
+# Write out the csv file
+m_data = []
+log_tweets = './' + 'tweets.csv'
+with open(log_tweets,'w') as outfile:
+    for line in tweets:
+        m_data.append(json.loads(json.dumps(line)))
+        outfile.write(json.dumps(line))
+        outfile.write("\n")
+        
+# Convert to a pandas dataframe
+tweetdb = pandas.DataFrame()
+tweetdb['id'] = list(map(lambda x: x['id'],m_data))
+tweetdb['created_at'] = list(map(lambda x: time.strftime('%Y-%m-%d %H:%M:%S', time.strptime(x['created_at'],'%a %b %d %H:%M:%S +0000 %Y')), m_data))
+tweetdb['text'] = list(map(lambda x: x['text'].encode('utf-8'), m_data))
+tweetdb['user'] = list(map(lambda x: x['user']['screen_name'], m_data))
+tweetdb['location'] = list(map(lambda x: x['user']['location'], m_data))
+tweetdb['followers'] = list(map(lambda x: x['user']['followers_count'], m_data))
+tweetdb['log_followers'] = list(map(lambda x: numpy.log(x['user']['followers_count']), m_data))
+
+header = ["created_at","followers"]
+tweetdb.to_csv('output.csv',columns=header)
+
+
+
+
+
+
+
+
+
+
